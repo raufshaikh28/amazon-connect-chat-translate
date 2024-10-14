3de5390e75bd70c84ae3323a1afc97b8411b4e05
Rajnikanthan06 <rajnirahul006@gmail.com>
2024-10-14 19:45:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Amplify }  from '@aws-amplify/core';
+import { Auth } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import awsconfig from './aws-exports';
